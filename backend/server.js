fc999e63bf3d9e7bca8bef32b1fd6b905917abb4
nanacode4 <y.xia2@universityofgalway.ie>
2025-06-24 16:33:40 +0100

    Updated URL

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,16 @@ if (!NASA_KEY) {
   console.warn("NASA_API_KEY is not set. Please check your Render environment variables.");
 }
 
+
 // Middleware for logging
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   next();
 });
+
+app.use(cors({
+  origin: 'https://nasa-frontend-kpp6.onrender.com'
+}));
 
 // Health check
 app.get("/api/health", (req, res) => {
